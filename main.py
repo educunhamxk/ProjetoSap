b6fabfda3548a48a5a0d56cf347622121b18e626
Eduardo Cunha <92655692+educunhamxk@users.noreply.github.com>
2023-09-29 16:05:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from page_feedback import feedback_page
 from dataframe import df
 import pandas as pd
 import streamlit as st
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 
 # Defina o tamanho da tela da web e o título
 st.set_page_config(
@@ -18,20 +18,13 @@ st.set_page_config(
 )
 
 with open("style.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 with st.sidebar:
     st.image("StockON.png")
     
-    selected = option_menu('', ['Analytics', 'Modelo de Previsão', 'Call to Action', 'Parâmetros', 'Feedback', 'Logoff'], 
-        icons=['file-earmark-bar-graph-fill', 'graph-up-arrow', 'check2-square', 'upload', 'send-check-fill', 'box-arrow-right'], menu_icon=" ", default_index=0,
-        styles={
-        "container": {"padding": "5!important", "background-color": "#fafafa"},
-        "icon": {"color": "#009EE2", "font-size": "22px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee", "font-family": "Segoe UI, sans-serif"},
-        "nav-link-selected": {"background-color": "#333333"},
-        }
-        )
+    options = ['Analytics', 'Modelo de Previsão', 'Call to Action', 'Parâmetros', 'Feedback', 'Logoff']
+    selected = st.selectbox('', options, index=0)
  
 if selected == 'Analytics':
     dashboard_page(df)
